e84721914bb3f41e275fa88ba7d223cecdd10744
zhaofei <zhaofeifei@vpiaotong.com>
2022-09-05 16:26:33 +0800

    first commit

---
+++ spring-beans/src/main/java/org/springframework/beans/factory/config/BeanExpressionResolver.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2002-2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.beans.factory.config;
+
+import org.springframework.beans.BeansException;
+import org.springframework.lang.Nullable;
+
+/**
+ * Strategy interface for resolving a value through evaluating it as an expression, if applicable.
+ * 通过将值作为表达式来解析值的决策略接口，如果适用
+ *
+ * <p>A raw {@link org.springframework.beans.factory.BeanFactory} does not
+ * contain a default implementation of this strategy. However,
+ * {@link org.springframework.context.ApplicationContext} implementations
+ * will provide expression support out of the box.
+ *
+ * 原始的BeanFactory没有该策略的默认实现，然而ApplicationContext的实现将提供开箱即用的表达式支持。
+ *
+ * @author Juergen Hoeller
+ * @since 3.0
+ */
+public interface BeanExpressionResolver {
+
+	/**
+	 * Evaluate the given value as an expression, if applicable;return the value as-is otherwise.
+	 * 如果适用，将给定值按照表达式进行计算，否则返回原值
+	 * @param value the value to check
+	 * @param evalContext the evaluation context
+	 * @return the resolved value (potentially the given value as-is)
+	 * @throws BeansException if evaluation failed
+	 */
+	@Nullable
+	Object evaluate(@Nullable String value, BeanExpressionContext evalContext) throws BeansException;
+
+}
---
+++ spring-context/src/test/java/self/TestApplication.java
@@ -0,0 +1,12 @@
+package self;
+
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestApplication {
+
+
+	public void testClassPathXmlApplication(){
+		ClassPathXmlApplicationContext classPathXmlApplicationContext = new ClassPathXmlApplicationContext("");
+	}
+}
